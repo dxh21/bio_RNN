731cd8e706a23df09ac8413bd7c943afda781a89
David He <he@stetter-itq.com>
2023-05-18 20:45:49 +0100

    STPCelldynamicz added

--- timegaps_and_input_sizes.py
+++ timegaps_and_input_sizes.py
@@ -225,6 +225,176 @@ class STPCell(nn.Module):
             self.h_t = torch.transpose(self.h_t, 0, 1)
             return self.h_t
 
+class STPCelldynamicz(nn.Module):
+    def __init__(self, input_size, hidden_size, complexity, e_h, alpha):
+        super(STPCelldynamicz, self).__init__()
+        self.input_size = input_size        
+        self.hidden_size = hidden_size
+        self.complexity = complexity 
+        sigmoid = nn.Sigmoid() 
+        self.ones = torch.ones(self.hidden_size, self.hidden_size)
+        self.batch_size = batch_size 
+        self.forprintingX = []
+        self.forprintingU = []
+        self.forprintingh = []
+
+        if self.complexity == "rich":
+            # System variables 
+            self.e_h = e_h
+
+            # Short term Plasticity variables 
+            self.delta_t = 1
+            self.alpha = alpha
+            self.e_ux = self.alpha * self.e_h
+            self.z_min = 0.001
+            self.z_max = 0.1
+
+            # Short term Depression parameters  
+            self.c_x = torch.nn.Parameter(torch.rand(self.hidden_size, self.hidden_size))
+
+            # Short term Facilitation parameters
+            self.c_u = torch.nn.Parameter(torch.rand(self.hidden_size, self.hidden_size))
+            self.c_U = torch.nn.Parameter(torch.rand(self.hidden_size, self.hidden_size))
+            
+            # System parameters            
+            self.w = torch.nn.Parameter(torch.rand(self.hidden_size, self.hidden_size))
+            self.p = torch.nn.Parameter(torch.rand(self.hidden_size, self.input_size))  
+            self.b = torch.nn.Parameter(torch.rand(self.hidden_size, 1))
+            self.scalar_alpha = torch.nn.Parameter(torch.ones(1))
+            self.scalar_beta = torch.nn.Parameter(torch.ones(1))
+            self.b_z = torch.nn.Parameter(torch.rand(self.hidden_size, 1))
+            
+            # State initialisations
+            self.h_t = torch.zeros(1, self.hidden_size, dtype=torch.float32)
+            self.X = torch.ones(self.hidden_size, self.hidden_size, dtype=torch.float32)     
+            self.U = torch.full((self.hidden_size, self.hidden_size), 0.9, dtype=torch.float32)         
+            self.Ucap = 0.9 * sigmoid(self.c_U)
+            self.Ucapclone = self.Ucap.clone().detach()
+
+        if self.complexity == "poor":
+            # System variables 
+            self.e_h = e_h
+
+            # Short term Plasticity variables 
+            self.delta_t = 1
+            self.alpha = alpha
+            self.e_ux = self.alpha * self.e_h
+            self.z_min = 0.001
+            self.z_max = 0.1
+
+            # Short term Depression parameters  
+            self.c_x = torch.nn.Parameter(torch.rand(self.hidden_size, 1))
+
+            # Short term Facilitation parameters
+            self.c_u = torch.nn.Parameter(torch.rand(self.hidden_size, 1))
+            self.c_U = torch.nn.Parameter(torch.rand(self.hidden_size, 1))
+            
+            # System parameters
+            self.w = torch.nn.Parameter(torch.rand(self.hidden_size, self.hidden_size))
+            self.p = torch.nn.Parameter(torch.rand(self.hidden_size, self.input_size))
+            self.b = torch.nn.Parameter(torch.rand(self.hidden_size, 1))
+            self.scalar_alpha = torch.nn.Parameter(torch.ones(1))
+            self.scalar_beta = torch.nn.Parameter(torch.ones(1))
+            self.b_z = torch.nn.Parameter(torch.rand(self.hidden_size, 1))
+            
+            # State initialisations
+            self.h_t = torch.zeros(1, self.hidden_size, dtype=torch.float32)
+            self.X = torch.ones(self.hidden_size, 1, dtype=torch.float32)
+            self.U = torch.full((self.hidden_size, 1), 0.9, dtype=torch.float32)
+            self.Ucap = 0.9 * sigmoid(self.c_U)
+            self.Ucapclone = self.Ucap.clone().detach()
+
+        for name, param in self.named_parameters():
+            #print(name, param.size(), param)
+            nn.init.uniform_(param, a=-(1/math.sqrt(self.hidden_size)), b=(1/math.sqrt(self.hidden_size)))
+            if name == "c_x":
+                nn.init.zeros_(param)
+            if name == "c_u":
+                nn.init.zeros_(param)
+            if name == "c_U":
+                nn.init.zeros_(param) 
+            if name == "p":
+                nn.init.uniform_(param, a=-(1/math.sqrt(self.input_size)), b=(1/math.sqrt(self.input_size)))     
+            if name == "b": 
+                nn.init.uniform_(param, a=-1, b=1)
+
+    def forward(self, x):                    
+        if self.complexity == "rich":
+            if self.h_t.dim() == 3:
+                self.h_t = self.h_t[0]
+            self.h_t = torch.transpose(self.h_t, 0, 1)
+            x = torch.transpose(x, 0, 1)
+            sigmoid = nn.Sigmoid()
+            
+            # graph plotting 
+            '''self.forprintingX.append(self.X[20,11,24].item())
+            self.forprintingU.append(self.U[20,11,24].item())
+            self.forprintingh.append(self.h_t[11, 20].item())
+            if len(self.forprintingX) % (196*5) == 0:
+                self.forprintingX = []
+                self.forprintingU = []
+                self.forprintingh = []'''   
+
+            # Short term Depression 
+            self.z_x = self.z_min + (self.z_max - self.z_min) * sigmoid(self.c_x)
+            self.X = self.z_x + torch.mul((1 - self.z_x), self.X) - self.delta_t * self.U * torch.einsum("ijk, ji  -> ijk", self.X, self.h_t)
+
+            # Short term Facilitation 
+            self.z_u = self.z_min + (self.z_max - self.z_min) * sigmoid(self.c_u)    
+            self.Ucap = 0.9 * sigmoid(self.c_U)
+            self.U = self.Ucap * self.z_u + torch.mul((1 - self.z_u), self.U) + self.delta_t * self.Ucap * torch.einsum("ijk, ji  -> ijk", (1 - self.U), self.h_t)
+            self.Ucapclone = self.Ucap.clone().detach() 
+            self.U = torch.clamp(self.U, min=self.Ucapclone.repeat(self.batch_size, 1, 1), max=torch.ones_like(self.Ucapclone.repeat(self.batch_size, 1, 1)))
+
+            # System Equations 
+            #self.z_h = 0.01 + (self.e_h-0.01) * sigmoid(self.c_h) 
+            self.absolute_w = torch.abs(self.w)
+            self.absolute_p = torch.abs(self.p)
+            self.z_h = sigmoid(self.scalar_alpha * torch.matmul(self.absolute_w, self.h_t) + self.scalar_beta * torch.matmul(self.absolute_p, x) + self.b_z)
+            x = torch.transpose(x, 0, 1)
+            self.h_t = torch.mul((1 - self.z_h), self.h_t) + self.z_h * sigmoid(torch.einsum("ijk, ki  -> ji", (self.w * self.U * self.X), self.h_t) + torch.matmul(self.p, x) + self.b)
+            self.h_t = torch.transpose(self.h_t, 0, 1)
+            return self.h_t   
+
+        if self.complexity == "poor":
+            if self.h_t.dim() == 3:
+                self.h_t = self.h_t[0]
+            self.h_t = torch.transpose(self.h_t, 0, 1)
+            x = torch.transpose(x, 0, 1)
+            sigmoid = nn.Sigmoid()
+            
+            # Short term Depression 
+            self.z_x = self.z_min + (self.z_max - self.z_min) * sigmoid(self.c_x)
+            self.X = self.z_x + torch.mul((1 - self.z_x), self.X) - self.delta_t * self.U * self.X * self.h_t
+
+            # Short term Facilitation 
+            self.z_u = self.z_min + (self.z_max - self.z_min) * sigmoid(self.c_u)    
+            self.Ucap = 0.9 * sigmoid(self.c_U)
+            self.U = self.Ucap * self.z_u + torch.mul((1 - self.z_u), self.U) + self.delta_t * self.Ucap * (1 - self.U) * self.h_t
+            self.Ucapclone = self.Ucap.clone().detach()
+            self.U = torch.clamp(self.U, min=self.Ucapclone.repeat(1, x.size(0)).to(device), max=torch.ones_like(self.Ucapclone.repeat(1, x.size(0)).to(device)))
+
+            # graph plotting 
+            '''self.forprintingX.append(self.X[20,5].item())
+            self.forprintingU.append(self.U[20,5].item())
+            if len(self.forprintingX) % 140 == 0:
+                self.forprintingX = []
+                self.forprintingU = []'''
+
+            # System Equations 
+            self.z_h = 0.01 + (self.e_h-0.01) * sigmoid(self.c_h)
+            # self.z_h = self.e_h * sigmoid(self.c_h) 
+            #a = self.w * self.U * self.X
+            #print("size of a", a.size())
+            #print("size of h_t", self.h_t.size())
+            #print("size of a * h_t", torch.matmul(a, self.h_t).size())
+            #print("size of x", x.size())
+            x = torch.transpose(x, 0, 1)
+            self.h_t = torch.mul((1 - self.z_h), self.h_t) + self.z_h * sigmoid(torch.matmul(self.w, (self.U * self.X * self.h_t)) + torch.matmul(self.p, x) + self.b)
+            #self.h_t = torch.matmul(self.w, self.h_t) + torch.matmul(self.p, x) + self.b
+            self.h_t = torch.transpose(self.h_t, 0, 1)
+            return self.h_t
+
 class STP(nn.Module):
     def __init__(self, input_size, hidden_size, complexity, e_h, alpha): 
         super(STP, self).__init__()
@@ -242,7 +412,7 @@ class RNN(nn.Module):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.lstm = STP(input_size, hidden_size, "poor", 0.9, 0.1)
+        self.lstm = STP(input_size, hidden_size, "rich", 0.9, 0.1)
         self.fc = nn.Linear(hidden_size, num_classes)
         self.update_number = 0
         pass
